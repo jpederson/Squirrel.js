c860e24f050ad1f5cab36881d90e83706db6e7a9
James Pederson <james@jpederson.com>
2015-08-02 11:44:01 -0500

    Bump version for release.

--- jquery.squirrel.js
+++ jquery.squirrel.js
@@ -3,7 +3,7 @@
  * http://github.com/jpederson/Squirrel.js
  * Author: James Pederson (jpederson.com)
  * Licensed under the MIT, GPL licenses.
- * Version: 0.1.6
+ * Version: 0.1.7
  */
 ; (function($, window, document, undefined) {
 
